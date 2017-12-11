605e2fc47d2437fda5082a88ce2b6d73b4799406
Jonatan Männchen <jonatan@maennchen.ch>
2017-12-11 11:58:26 +0100

    Mix Init

---
+++ lib/ex_saferpay.ex
@@ -0,0 +1,18 @@
+defmodule ExSaferpay do
+  @moduledoc """
+  Documentation for ExSaferpay.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExSaferpay.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_saferpay_test.exs
@@ -0,0 +1,8 @@
+defmodule ExSaferpayTest do
+  use ExUnit.Case
+  doctest ExSaferpay
+
+  test "greets the world" do
+    assert ExSaferpay.hello() == :world
+  end
+end
